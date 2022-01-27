d791b9c5428acfe663704fcdd86ab32c2395d876
Shubhodeep Banerjee <sshubhodeepp@gmail.com>
2022-01-27 18:06:28 +0530

    4th Commit - Image and Basket Button

--- src/Product.js
+++ src/Product.js
@@ -3,15 +3,17 @@ import React from 'react';
 function Product() {
   return <div className='product'>
             <div className='product__info'>
-                <p>The Lean Startup</p>
+                <p>The New Amazon Alexa</p>
                 <p className='product__price'>
                     <small>$</small>
-                    <strong>19.29</strong>
+                    <strong>100.29</strong>
                 </p>
                 <div className='product__rating'>
                     <p>⭐️⭐️⭐️⭐️</p>
                 </div>
             </div>
+        <img src='https://www.wordstream.com/wp-content/uploads/2021/07/amazon-seo-product-images.jpg' alt=''/>
+        <button>Add to Basket</button>
   </div>;
 }
 
