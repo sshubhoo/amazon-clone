2a6bdd470e8dbd7ad386bf2873729add871b6405
Shubhodeep Banerjee <sshubhodeepp@gmail.com>
2022-01-27 22:05:17 +0530

    Commit 10 - Basket Value Updated

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { useStateValue } from "./StateProvider";
 import { Link } from 'react-router-dom';
 
 function Header() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
   return <div className="header">
             <Link to="/">
                 <img className="header_logo" src="https://www.bizmonthly.com/wp-content/uploads/2020/04/Amazon-logo-black-template.png"></img>
@@ -30,7 +33,7 @@ function Header() {
                 <Link to="/checkout">
                     <div className="header_optionBasket">
                         <ShoppingBasketIcon/>
-                        <span className="header_optionLineTwo header_basketCount">0</span>
+                        <span className="header_optionLineTwo header_basketCount">{basket?.length}</span>
                     </div>
                 </Link>
     
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer, { initialState } from './reducer';
+import {StateProvider} from './StateProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     {/*The below step is done , so that every component gets access to the data layer*/}
-    <StateProvider initialState={initialState} reducer={reducer}>
+    <StateProvider initialState={initialState} reducer=
+    {reducer}>
       <App />
-    </StateProvider>
+    </StateProvider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
