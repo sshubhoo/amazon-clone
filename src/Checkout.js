a00646e9d47bc1322e91261132c074246d2d65c8
Shubhodeep Banerjee <sshubhodeepp@gmail.com>
2022-01-27 23:53:25 +0530

    Commit 11 - Remove From Basket Functionality Done

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from './Subtotal';
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 {/*Imported npm i react-currecy-format*/}
 
 function Checkout() {
+
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return <div className='checkout'>
       <div className='checkout__left'>
         <img className='checkout__ad' src='https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/CorpGC/Banner_PC_without_Citi.jpg' alt=''/>
@@ -11,6 +16,15 @@ function Checkout() {
             <h2 className='checkout__title'>
                 Your Shopping Basket
             </h2>
+            {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className='checkout__right'>
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './reducer';
+import { useStateValue } from './StateProvider';
 
 function Subtotal() {
+
+  const [{basket}, dispatch] = useStateValue();
+
   return <div className='subtotal'>
         <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
               {/* Part of the homework */}
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -17,7 +22,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={getBasketTotal(basket)} // Part of the homework
         displayType={"text"}
         thousandSeparator={true}
         prefix={"₹"}
