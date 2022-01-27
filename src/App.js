a3e73a9c05e44551cc5f87d459ba7d76016b45b1
Shubhodeep Banerjee <sshubhodeepp@gmail.com>
2022-01-27 17:28:27 +0530

    2nd Commit - Created the Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './Header.js'
 import Header from './Header.js';
+import Home from './Home';
 
 function App() {
   return (
     //BEM naming convention used
     <div className="app">
       <Header/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return <div className="home">
+      <div className="home__container">
+          <img className="home_image" src="https://m.media-amazon.com/images/I/81RoKL-XM-L._SX3000_.jpg" alt=''/>
+          <div className="home__container">
+            {/*Product*/}
+            {/*Product*/}
+          </div>
+          <div className="home__container">
+            {/*Product*/}
+            {/*Product*/}
+            {/*Product*/}
+          </div>
+          <div className="home__container">
+            {/*Product*/}
+          </div>
+      </div>
+  </div>;
+}
+
+export default Home;
